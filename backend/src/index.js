a12ff20535e9194974ae474ddca3eee840df16cd
kanikamishra05 <kanikarocks11@gmail.com>
2026-06-08 17:57:30 +0530

    Add Vercel frontend URL to CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://realtime-chat-application-rust.vercel.app"
+    ],
     
     credentials: true,
   })
